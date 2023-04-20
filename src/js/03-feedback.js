f2b2f9d8f9608e93ee671b139d0d86a161640457
AlexSanako <116287214+AlexSanako@users.noreply.github.com>
2023-04-20 15:11:52 +0300

    03

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,2 +1,38 @@
 import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+const emailInput = form.elements.email;
+const messageInput = form.elements.message;
+
+const FEEDBACK_FORM_STATE_KEY = 'feedback-form-state';
+
+const saveStateToLocalStorage = throttle(() => {
+  const state = {
+    email: emailInput.value,
+    message: messageInput.value,
+  };
+  localStorage.setItem(FEEDBACK_FORM_STATE_KEY, JSON.stringify(state));
+}, 500);
+
+const getStateFromLocalStorage = () => {
+  const savedState = localStorage.getItem(FEEDBACK_FORM_STATE_KEY);
+  if (savedState) {
+    const state = JSON.parse(savedState);
+    emailInput.value = state.email;
+    messageInput.value = state.message;
+  }
+};
+
+form.addEventListener('input', saveStateToLocalStorage);
+window.addEventListener('load', getStateFromLocalStorage);
+form.addEventListener('submit', e => {
+  e.preventDefault();
+  const state = {
+    email: emailInput.value,
+    message: messageInput.value,
+  };
+  console.log(state);
+  localStorage.removeItem(FEEDBACK_FORM_STATE_KEY);
+  emailInput.value = '';
+  messageInput.value = '';
+});
