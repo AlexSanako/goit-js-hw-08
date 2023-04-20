404a2cccc9a0ba4962b7902c18736ed6b6c5c6a5
AlexSanako <116287214+AlexSanako@users.noreply.github.com>
2023-04-20 14:28:19 +0300

    02

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,22 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+console.log(Player);
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+const THROTTLE_DELAY = 1000; // інтервал для збереження часу в сховище
+const saveCurrentTime = throttle(currentTime => {
+  localStorage.setItem('videoplayer-current-time', currentTime);
+}, THROTTLE_DELAY);
+
+player.on('timeupdate', event => {
+  const currentTime = event.seconds;
+  saveCurrentTime(currentTime);
+});
+
+const savedTime = localStorage.getItem('videoplayer-current-time');
+if (savedTime) {
+  player.setCurrentTime(savedTime);
+}
